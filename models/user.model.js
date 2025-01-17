9c59525d262eb75935707b75d7ffc4702cffaca1
OKE Erwin <okeerwin5@gmail.com>
2025-01-17 22:03:02 +0000

    formatage des fichiers

--- models/user.model.js
+++ models/user.model.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-{
-    username: {
-        type: String,
-        require: true,
-    },
-    password: {
-        type: String,
-        require: true,
-    }
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
 });
 
-const Utilisateur = mongoose.model('Utilisateur', userSchema);
+const Utilisateur = mongoose.model("Utilisateur", userSchema);
 module.exports = Utilisateur;
--- server.js
+++ server.js
@@ -1,31 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const authRoutes = require('./routes/authRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, "views")));
 
 //route get pour afficher la page de connexion/inscription
 app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname,'views', 'index.html'));
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-mongoose.connect(process.env.DB_URL
-).then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
   });
 
-}).catch((err) => {
-  console.log('Failed to connect to MongoDB', err);
-});
-
-
 //routes
-app.use("/",authRoutes)
-
+app.use("/", authRoutes);
