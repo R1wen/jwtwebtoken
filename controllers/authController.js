ad0272ec2da0c268f568e8ab57e0e32b0eb66dee
OKE Erwin <okeerwin5@gmail.com>
2025-01-17 21:27:40 +0000

    premier depot sur les teste de bcrypt et jwt

---
+++ controllers/authController.js
@@ -0,0 +1,75 @@
+const bcrypt = require("bcrypt");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+module.exports.connection = (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/index.html"));
+};
+
+module.exports.private = (req, res) => {
+  res.json({ title: "mon premier titre", description: "HOPA GANGNAM STYLE!" });
+};
+
+module.exports.signup = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    //Vérifier si tous les champs sont remplis
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ error: "Tous les champs sont obligatoires." });
+    }
+
+    // Vérifier si l'utilisateur existe déjà
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ error: "Le nom d'utilisateur existe déjà." });
+    }
+
+    // Hasher le mot de passe
+    const hashedPassword = await bcrypt.hash(password, 10); // Salt rounds = 10
+
+    //Créer un nouvel utilisateur
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+
+    // Sauvegarder dans la base de données
+    await newUser.save();
+
+    res.status(201).send("Utilisateur créé avec succès.");
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la création de l'utilisateur." });
+  }
+};
+
+module.exports.login = async (req, res) => {
+  //VERIFIER SI L'utillisateur existe
+  const existingUser = await User.findOne({ username: req.body.username });
+  if (!existingUser) {
+    return res
+      .status(400)
+      .json({ error: "Le nom d'utilisateur n'existe pas." });
+  }
+
+  //verifier si le mot de passe est correcte
+  const passwordCorrect = await bcrypt.compare(
+    req.body.password,
+    existingUser.password
+  );
+  if (!passwordCorrect) {
+    return res.status(400).json({ error: "Le mot de passe est incorrecte." });
+  }
+
+  //Créer et assigner un token
+  const token = jwt.sign({ _id: existingUser._id }, process.env.TOKEN_SECRET);
+  res.header("auth-token", token).send(token);
+};
---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+const { Router } = require('express')
+const authController = require('../controllers/authController')
+const verify = require("../routes/verifyToken")
+
+const router = Router()
+
+router.get('/connection', (req,res) => authController.connection(req,res))
+router.get('/hopa' , verify , (req,res) => authController.private(req,res))
+router.post('/signup', (req,res) => authController.signup(req,res))
+router.post('/login', (req,res) => authController.login(req,res))
+
+module.exports = router
